c45523a6b29d8db68ca3d1d5910f100414664bcc
Steve McDougall <juststevemcd@gmail.com>
2021-08-09 13:42:12 +0100

    PSR-12 compliance

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Repositories\ConfigRepository;
+use Illuminate\Filesystem\Filesystem;
 use Tests\TestCase;
 use Tests\CreatesApplication;
 
 uses(TestCase::class, CreatesApplication::class)
     ->beforeEach(function () {
-        //
+        (new Filesystem)->deleteDirectory(base_path('tests/.fathom'));
+
+        $this->config = resolve(ConfigRepository::class)->set('token', '123123213');
+    })->afterEach(function () {
+        (new Filesystem)->deleteDirectory(base_path('tests/.fathom'));
     })->in('Feature', 'Unit');
